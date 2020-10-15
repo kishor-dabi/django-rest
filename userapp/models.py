852b526e7e4f9561eee0de5db936eec948b765b8
kishor dabi <42381234+kishor-dabi@users.noreply.github.com>
2020-10-15 21:30:50 +0530

    Update models.py

--- userapp/models.py
+++ userapp/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 import uuid
 from django.db import models
-from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
 from django_rest.userrole.models import UserRole
 
 class UserManager(BaseUserManager):
@@ -37,7 +37,7 @@ class UserManager(BaseUserManager):
         return user
 
 
-class User(AbstractBaseUser):
+class User(AbstractBaseUser, PermissionsMixin):
 
     # id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     email = models.EmailField(
